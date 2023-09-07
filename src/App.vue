a19fbcb3e9f27ecb5b7bffa8740a6e54fae88d5d
Ben Topping <ben.topping@outlook.com>
2023-09-07 21:36:01 +0100

    feat: Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue';
+</script>
+
+<template>
+  <div class="flex flex-col min-h-screen p-8">
+    <NavBar />
+    <RouterView />
+  </div>
+</template>
+
---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import IconScience from './icons/IconScience.vue';
+</script>
+
+<template>
+  <div class="flex w-full align-items border-b-2 pb-4">
+    <div class="flex flex-row gap-x-4 items-center">
+      <IconScience height="96" width="96" class="min-w-[96px]"/>
+      <div class="flex flex-col gap-y-2">
+        <h1 class="text-5xl font-light">Vue Experiments</h1>
+        <span class="text-gray-700 text-wrap max-w-lg">
+          A series of drop-in Vue Single-File Components written in typescript
+          with the aim of pushing my Vue abilities
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
