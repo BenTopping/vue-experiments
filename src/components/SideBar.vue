1bc399fc62024ea6d04ac11b8a7f3f1242b14707
Ben Topping <ben.topping@outlook.com>
2024-06-16 14:41:06 +0100

    chore(deps): updates deps to latest versions

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -9,20 +9,18 @@ function clickView(view: string) {
 </script>
 
 <template>
-  <transition>
-    <aside class="grow min-h-full border-r-2 pr-4 space-y-2">
-      <button
-        class="flex rounded-md bg-gray-100 w-64 text-xl p-4 select-none cursor-pointer hover:text-white hover:bg-black transition duration-75"
-        @click="clickView('FlipClockView')"
-      >
-        Flip clock
-      </button>
-      <button
-        class="flex rounded-md bg-gray-100 w-64 text-xl p-4 select-none cursor-pointer hover:text-white hover:bg-black transition duration-75"
-        @click="clickView('CodeBlockView')"
-      >
-        Code block
-      </button>
-    </aside>
-  </transition>
+  <aside class="grow min-h-full border-r-2 pr-4 space-y-2">
+    <button
+      class="flex rounded-md bg-gray-100 w-64 text-xl p-4 select-none cursor-pointer hover:text-white hover:bg-black transition duration-75"
+      @click="clickView('FlipClockView')"
+    >
+      Flip clock
+    </button>
+    <button
+      class="flex rounded-md bg-gray-100 w-64 text-xl p-4 select-none cursor-pointer hover:text-white hover:bg-black transition duration-75"
+      @click="clickView('CodeBlockView')"
+    >
+      Code block
+    </button>
+  </aside>
 </template>
