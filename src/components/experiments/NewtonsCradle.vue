78376824b464f651003f28ddd75223ed83d0431b
Ben Topping <ben.topping@outlook.com>
2025-03-26 18:15:02 +0000

    feat(newtons-cradle): removes javascript and uses CSS only

--- src/components/experiments/NewtonsCradle.vue
+++ src/components/experiments/NewtonsCradle.vue
@@ -1,61 +1,65 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-
-const leftball = ref(true)
-
-onMounted(() => {
-  setInterval(() => {
-    leftball.value = !leftball.value
-  }, 1100)
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div class="flex flex-row">
-    <div class="flex flex-col grow">
-      <div class="w-full h-2 bg-black rounded-full p-2 relative top-1"></div>
-      <div class="flex flex-row">
-        <div :class="['flex flex-col items-center', { leftball }]">
-          <div class="flex bg-black w-1 h-32"></div>
-          <div class="flex w-8 h-8 bg-black rounded-full border-2 border-black"></div>
-        </div>
-        <div v-for="index in 5" :key="index">
-          <div class="flex flex-col items-center">
+    <div class="flex flex-row grow">
+      <div
+        class="flex h-full bg-black w-2 rotate-15 rounded-full items-center justify-center"
+      ></div>
+      <div class="flex flex-col">
+        <div class="w-full h-2 bg-black rounded-full relative top-2"></div>
+        <div class="flex flex-row">
+          <div class="flex flex-col items-center leftball">
+            <div class="flex bg-black w-1 h-32"></div>
+            <div class="flex w-8 h-8 bg-black rounded-full border-2 border-black"></div>
+          </div>
+          <div v-for="index in 3" :key="index">
+            <div class="flex flex-col items-center">
+              <div class="flex bg-black w-1 h-32"></div>
+              <div class="flex w-8 h-8 bg-black rounded-full border-2 border-black"></div>
+            </div>
+          </div>
+          <div class="flex flex-col items-center rightball">
             <div class="flex bg-black w-1 h-32"></div>
             <div class="flex w-8 h-8 bg-black rounded-full border-2 border-black"></div>
           </div>
         </div>
-        <div :class="['flex flex-col items-center', { rightball: !leftball }]">
-          <div class="flex bg-black w-1 h-32"></div>
-          <div class="flex w-8 h-8 bg-black rounded-full border-2 border-black"></div>
-        </div>
       </div>
+      <div
+        class="flex h-full bg-black w-2 -rotate-15 rounded-full items-center justify-center"
+      ></div>
     </div>
   </div>
 </template>
 
 <style>
 .leftball {
-  animation: leftball 1.2s cubic-bezier(0.5, 0, 0.5, 1) both;
+  animation: leftball 2.5s cubic-bezier(0.5, 0, 0.5, 1) infinite both;
   transform: rotate(0deg);
   transform-origin: 50% 0%;
 }
 
 .rightball {
-  animation: rightball 1.2s cubic-bezier(0.5, 0, 0.5, 1) both;
+  animation: rightball 2.5s cubic-bezier(0.5, 0, 0.5, 1) 1.25s infinite both;
   transform: rotate(0deg);
   transform-origin: 50% 0%;
 }
 
 @keyframes leftball {
+  25% {
+    transform: rotate(75deg);
+  }
   50% {
-    transform: rotate(60deg);
+    transform: rotate(0deg);
   }
 }
 
 @keyframes rightball {
+  25% {
+    transform: rotate(-75deg);
+  }
   50% {
-    transform: rotate(-60deg);
+    transform: rotate(0deg);
   }
 }
 </style>
--- src/views/NewtonsCradleView.vue
+++ src/views/NewtonsCradleView.vue
@@ -12,67 +12,72 @@ const dependencies: IDependency[] = []
 const currentTab = ref('experiment')
 
 const codeBlockString: string = `
-<script setup lang="ts">
-import { ref, onMounted } from 'vue'
-
-const leftball = ref(true)
-
-onMounted(() => {
-  setInterval(() => {
-    leftball.value = !leftball.value
-  }, 1100)
-})
-<\/script>
+<script setup lang="ts"><\/script>
 
 <template>
   <div class="flex flex-row">
-    <div class="flex flex-col grow">
-      <div class="w-full h-2 bg-black rounded-full p-2 relative top-1"></div>
-      <div class="flex flex-row">
-        <div :class="['flex flex-col items-center', { leftball }]">
-          <div class="flex bg-black w-1 h-32"></div>
-          <div class="flex w-8 h-8 bg-black rounded-full border-2 border-black"></div>
-        </div>
-        <div v-for="index in 5" :key="index">
-          <div class="flex flex-col items-center">
+    <div class="flex flex-row grow">
+      <div
+        class="flex h-full bg-black w-2 rotate-15 rounded-full items-center justify-center"
+      ></div>
+      <div class="flex flex-col">
+        <div class="w-full h-2 bg-black rounded-full relative top-2"></div>
+        <div class="flex flex-row">
+          <div class="flex flex-col items-center leftball">
+            <div class="flex bg-black w-1 h-32"></div>
+            <div class="flex w-8 h-8 bg-black rounded-full border-2 border-black"></div>
+          </div>
+          <div v-for="index in 3" :key="index">
+            <div class="flex flex-col items-center">
+              <div class="flex bg-black w-1 h-32"></div>
+              <div class="flex w-8 h-8 bg-black rounded-full border-2 border-black"></div>
+            </div>
+          </div>
+          <div class="flex flex-col items-center rightball">
             <div class="flex bg-black w-1 h-32"></div>
             <div class="flex w-8 h-8 bg-black rounded-full border-2 border-black"></div>
           </div>
         </div>
-        <div :class="['flex flex-col items-center', { rightball: !leftball }]">
-          <div class="flex bg-black w-1 h-32"></div>
-          <div class="flex w-8 h-8 bg-black rounded-full border-2 border-black"></div>
-        </div>
       </div>
+      <div
+        class="flex h-full bg-black w-2 -rotate-15 rounded-full items-center justify-center"
+      ></div>
     </div>
   </div>
 </template>
 
 <style>
 .leftball {
-  animation: leftball 1.2s cubic-bezier(0.5, 0, 0.5, 1) both;
+  animation: leftball 2.5s cubic-bezier(0.5, 0, 0.5, 1) infinite both;
   transform: rotate(0deg);
   transform-origin: 50% 0%;
 }
 
 .rightball {
-  animation: rightball 1.2s cubic-bezier(0.5, 0, 0.5, 1) both;
+  animation: rightball 2.5s cubic-bezier(0.5, 0, 0.5, 1) 1.25s infinite both;
   transform: rotate(0deg);
   transform-origin: 50% 0%;
 }
 
 @keyframes leftball {
+  25% {
+    transform: rotate(75deg);
+  }
   50% {
-    transform: rotate(60deg);
+    transform: rotate(0deg);
   }
 }
 
 @keyframes rightball {
+  25% {
+    transform: rotate(-75deg);
+  }
   50% {
-    transform: rotate(-60deg);
+    transform: rotate(0deg);
   }
 }
 </style>
+
 `
 </script>
 
@@ -81,12 +86,14 @@ onMounted(() => {
     <div class="flex flex-col mb-4 text-xl">
       <h2 class="text-5xl font-light mb-4">Newton's Cradle</h2>
       <div class="flex flex-col space-y-2">
-        <span>Mimicking conservation of momentum with Vue animations</span>
+        <span>Mimicking conservation of momentum with CSS animations.</span>
         <span>
           An animated version of the classic Newton's Cradle toy. An alternative would be to use
           something like
           <a href="https://brm.io/matter-js/" target="_blank" class="font-bold"> Matter.js </a> for
-          a more realistic physics simulation.
+          a more realistic physics simulation. This ended up using no Vue / Javascript and just CSS
+          animations. Originally I used Vue to toggle the applied css classes to make the animation
+          infinite but this can be done more easily using CSS.
         </span>
         <div class="flex flex-row space-x-2 py-4">
           <span>Dependencies:</span>
